12e68b43cf92e3b7fb48e6ac6f97788981e1cd9e
Guan <guan865182221@gmail.com>
2020-10-21 16:56:14 -0500

    Update 03_Python_Calc.py

--- 03_Python_Calc.py
+++ 03_Python_Calc.py
@@ -7,8 +7,13 @@ total = price * weight
 print (total)
 """
 #简化版本
+“”“
 price = float(input("apple:"))
 weight = float(input("weight:"))
 total = price * weight
 print (total)
-
+”“”
+apple = float(input("apple:"))
+weight = float(input("weight:"))
+total = apple * weight
+print ("apple %f .08 $/KG, weight %.01f kg, total %.01f"(apple, weight, total))
