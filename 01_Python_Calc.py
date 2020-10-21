e52a40ee61a841248c6b000e7ff51df96d0db348
Guan <guan865182221@gmail.com>
2020-10-21 16:18:39 -0500

    Update 01_Python_Calc.py

--- 01_Python_Calc.py
+++ 01_Python_Calc.py
@@ -1,7 +1,14 @@
+"""
 price_str = input("apple:")
 weight_str = input("weight:")
 price = float(price_str)
 weight = float(weight_str)
+total = price * weight
+print (total)
+"""
+#简化版本
+price = float(input("apple:"))
+weight = float(input("weight:"))
 total = price * weight
 print (total)
 
