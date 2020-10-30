091a658301d0427f1b9b50beebb202090984dda3
Guan <guan865182221@gmail.com>
2020-10-30 14:50:33 -0500

    Update 05_石头剪刀布.py

--- 05_石头剪刀布.py
+++ 05_石头剪刀布.py
@@ -1,3 +1,3 @@
-player = int(input("你要出的是 石头 (1) /剪刀 (2) /布 (3) : ”））
+player = int(input("你要出的是 石头 (1) /剪刀 (2) /布 (3) : "))
 
-print（“你出的拳头是 %d” % player)
+print("the player choice is %d" % player)
