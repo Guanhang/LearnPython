d6b21bb2dcad4bb10ea166db612209eb702566c4
Guan <guan865182221@gmail.com>
2020-10-30 17:16:30 -0500

    Create 05_数字判断.py

---
+++ 05_数字判断.py
@@ -0,0 +1,14 @@
+import random
+
+number = random.randint(1,100)
+
+print(number)
+
+if number > 80 :
+    print("Higher than eighty")
+
+elif number > 50:
+    print("Higher than fifty")
+
+elif number > 20:
+    print("Higher than twenty")
