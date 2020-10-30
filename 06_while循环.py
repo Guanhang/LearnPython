775ca9c2fb33f0433289265ce26ce79c39cca149
Guan <guan865182221@gmail.com>
2020-10-30 17:40:28 -0500

    Create 06_while循环.py

---
+++ 06_while循环.py
@@ -0,0 +1,15 @@
+"""
+条件 ＝ 1
+
+while 条件（判断 计数器 是否达到 目标次数）：
+    条件满足，做到事情
+    条件满足，做到事情
+    处理条件（计数器＋1）
+“”“
+
+res = 1
+
+while res <=10:
+    print("Hello world")
+    res = res + 3
+print("res= %d" % res)
