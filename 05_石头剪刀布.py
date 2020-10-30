9113a0e91a09bdadc2cefb2a434a98d3194a19b8
Guan <guan865182221@gmail.com>
2020-10-30 16:35:49 -0500

    Update 05_石头剪刀布.py

--- 05_石头剪刀布.py
+++ 05_石头剪刀布.py
@@ -4,9 +4,14 @@ computer = 1
 
 print("the player choice is %d - the computer choice is %d" % (player, computer))
 
-if (player == 1 and computer == 2) or (player == 2 and computer == 3) or (player == 3 and computer == 1):
+if ((player == 1 and computer == 2) 
+        or (player == 2 and computer == 3) 
+        or (player == 3 and computer == 1)):
+        
     print("Player Win")
+    
 elif player == computer:
     print("Again")
+
 else:
     print("Player lose")
