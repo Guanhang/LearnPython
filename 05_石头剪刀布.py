62bb2742404b061afb09742b26910161057bb2c7
Guan <guan865182221@gmail.com>
2020-10-30 14:59:59 -0500

    Update 05_石头剪刀布.py

--- 05_石头剪刀布.py
+++ 05_石头剪刀布.py
@@ -3,3 +3,6 @@ player = int(input("你要出的是 石头 (1) /剪刀 (2) /布 (3) : "))
 computer = 1
 
 print("the player choice is %d - the computer choice is %d" % (player, computer))
+
+if (player == 1 and computer == 2) or (player == 2 and computer == 3) or (player == 3 and computer == 1):
+    print("You Win")
