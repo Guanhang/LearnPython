3ec689b53be3fd49dfa5901e6687518aaabf8b31
Guan <guan865182221@gmail.com>
2020-11-01 15:31:23 -0500

    Update 06_while循环_0到100循环相加.py

--- 06_while循环_0到100循环相加.py
+++ 06_while循环_0到100循环相加.py
@@ -5,3 +5,17 @@ while i <= 100:
     i += 1
     result += i
 print("The accumulation number is %d" %result)
+
+
+#取偶数相加
+"""
+i = 0
+result = 0
+while i <= 100:
+    if i % 2 == 0:
+        print(i)
+        result += i
+    i += 1
+print("the number of result is %d" % result)
+"""
+
